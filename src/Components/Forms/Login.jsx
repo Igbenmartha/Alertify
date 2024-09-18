397a04afb76a7e3fd0ec91ecf14301cd57a5c5a9
Nini-123-coderigbenmartha <igbenfeji@gmaail.com>
2024-09-18 21:51:18 +0100

    redid the login api

--- src/Components/Forms/Login.jsx
+++ src/Components/Forms/Login.jsx
@@ -56,11 +56,12 @@ const Login = () => {
             setLoading(false); 
           }, 2000);
         } catch (error) {
-          // console.log(error);
-          toast.error(error.response.data.data.message);
-          setLoading(false)
+          console.log(error);
+          toast.error(error.response.data.message);
+        setLoading(false)
           
         }
+
       }
 
   return (
